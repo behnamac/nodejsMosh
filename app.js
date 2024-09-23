7e918b28f99c4682b359da6e039125259cc333b0
Behnam <behnam.sep@gmail.com>
2024-09-23 09:45:30 +0200

    Os module

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const log = require("./modules/logger");
-log.myLogs("Behnam you're awesome");
+const os=require('os');
+
+const freeMemory=os.freemem();
+const totalMemory=os.totalmem();
+
+console.log(`Free Memory: ${freeMemory}`);
+console.log(`Total Memory: ${totalMemory}`);
--- modules/logger.js
+++
@@ -1,6 +0,0 @@
-var url = "https://www.google.com";
-
-function log(name) {
-  console.log(name);
-}
-module.exports.myLogs = log;
