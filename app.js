7c670d0cbde1575a31ff9300acb75fc28b6c85c3
Behnam <behnam.sep@gmail.com>
2024-09-23 13:29:18 +0200

    Event Arguments

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 //Register a Listener
-emitter.on("messageLogged", function () {
-  console.log("Listener Called");
+emitter.on("messageLogged", (arg) => {
+  console.log("Listener Called", arg);
 });
 
 //Raise an event
-emitter.emit('messageLogged');
+//emitter.emit('messageLogged',{id:1,message:"Hello"});
+
+//Raise :loggin(data:message)
+emitter.emit("messageLogged", "Hello Behnam");
